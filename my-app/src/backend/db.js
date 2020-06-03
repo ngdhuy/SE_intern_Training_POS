088cbeaa36c6006148f543a59198454e5de6aa78
Dao-Dung <daoadung69@gmail.com>
2020-06-03 13:45:46 +0700

    dung upload #012

--- my-app/src/backend/db.js
+++ my-app/src/backend/db.js
@@ -1,24 +1,4 @@
 
-// mongoose.connect('mongodb+srv://TaskApp:daoadung123@cluster0-jyznc.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true}) 
-
-// mongoose.connection.on('connected', function () {  
-  //   console.log('Mongoose default connection open');
-  // }); 
-  
-  // mongoose.connection.on('error',function (err) {  
-    //   console.log('Mongoose default connection error: ' + err);
-    // }); 
-    
-    // mongoose.connection.on('disconnected', function () {  
-      //   console.log('Mongoose default connection disconnected'); 
-      // });
- 
-      // process.on('SIGINT', function() {  
-//   mongoose.connection.close(function () { 
-  //     console.log('Mongoose default connection disconnected through app termination'); 
-  //     process.exit(0); 
-//   }); 
-// });
 
 
 const mongoose = require('mongoose');
